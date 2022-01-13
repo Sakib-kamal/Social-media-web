6f2c75d9a38b414663302bf24995c6d7ba175d22
sakib kamal <sakibuiet123@gmail.com>
2022-01-13 17:30:56 +0530

    first commit

--- RestApi/routes/posts.js
+++ RestApi/routes/posts.js
@@ -68,7 +68,7 @@ router.put("/:id", async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
       res.status(200).json(post);
-    } catch (err) {
+    } catch (err) {  
       res.status(500).json(err);
     }
   });
